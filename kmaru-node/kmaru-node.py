2e85e10ca912443842e6d501a5af7e878b77d07f
Paul Tagliamonte <paultag@ubuntu.com>
2011-02-02 13:58:48 -0500

    pushing in some goodies

---
+++ kmaru-node/kmaru-node.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# OK. Gametime.
+
+import kmaru.lcia
+
+r_payload          = {}
+r_payload['class'] = "auth"
+r_payload['sid']   = 2
+r_payload['time']  = 1234567890
+r_payload['data']  = {}
+r_payload['data']['uname'] = "tag"
+
+r_ack = kmaru.lcia.xml_r(r_payload)
+print r_ack;
---
+++ kmaru-node/kmaru/lcia.py
@@ -0,0 +1,34 @@
+#
+# GPL and shit
+#
+
+from xml.dom.minidom import Document
+
+root_node = "lcia"
+
+def xml_r( r_payload ):
+	try:
+		doc = Document()
+		lcia = doc.createElement("lcia")
+		doc.appendChild(lcia)
+
+		headers = doc.createElement("headers")
+		lcia.appendChild(headers)
+
+		klass = doc.createElement("class")
+		sid   = doc.createElement("sid")
+
+		headers.appendChild(klass)
+		headers.appendChild(sid)
+
+		class_text = doc.createTextNode(r_payload['class'])
+		sid_text   = doc.createTextNode(str(r_payload['sid']))
+
+		klass.appendChild(class_text)
+		sid.appendChild(sid_text)
+
+		xml_string = doc.toprettyxml(indent="  ")
+		return xml_string;
+	except KeyError:
+		print "Oh holyshit"
+	
