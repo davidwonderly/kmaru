ab55d902f406f3a739b558a7671062a2e485e435
Paul Tagliamonte <paultag@ubuntu.com>
2011-02-02 19:25:24 -0500

    cleaning up the headers about now

--- kmaru-node/kmaru/api.py
+++ kmaru-node/kmaru/api.py
@@ -5,39 +5,39 @@
 
 import time
 
-def auth( session, username, password ):
+def genPayload():
 	payload           = {}
 	payload['data']   = {}
 	payload['header'] = {}
-	payload['header']['sid']   = session
 	payload['header']['time']  = time.time()
+	return payload
+
+def auth( session, username, password ):
+	payload = genPayload()
+
+	payload['header']['sid']   = session
 	payload['header']['class'] = "AUTH"
 	payload['data']['uname']   = username 
 	payload['data']['ident']   = password
+
 	return payload
 
 
 def autha( session, errors, status, message ):
-	payload           = {}
-	payload['data']   = {}
-	payload['header'] = {}
+	payload = genPayload()
 
 	payload['header']['sid']    = session
-	payload['header']['time']   = time.time()
 	payload['header']['class']  = "AUTHA"
 	payload['header']['errors'] = errors
 	payload['header']['msg']    = message
-
 	payload['data']['status']   = status
+
 	return payload
 
 def errora( session, errors, message ):
-	payload           = {}
-	payload['data']   = {}
-	payload['header'] = {}
+	payload = genPayload()
 
 	payload['header']['sid']    = session
-	payload['header']['time']   = time.time()
 	payload['header']['class']  = "ERRORA"
 	payload['header']['errors'] = errors
 	payload['header']['msg']    = message
