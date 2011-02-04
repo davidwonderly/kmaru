dd92e5165572e929a2f7df3bc1857dea6568c3ba
Paul Tagliamonte <paultag@ubuntu.com>
2011-02-04 02:46:16 +0000

    doing awesomeness

--- kmaru-node/kmaru-server.py
+++ kmaru-node/kmaru-server.py
@@ -7,15 +7,21 @@
 import kmaru.server
 import twisted.internet.error
 
-port = 2017
+port    = 2017
+
+keydir  = "./ssl-keys"
+keyfile = "server.key"
+crtfile = "server.crt"
 
 print "We're running kmaru on port " + str(port)
+print " Searching for SSL keys in: " + keydir + "/"
+print "  ( looking for both " + keyfile + " and " + crtfile + " )"
 
 try:
 	kmaru.server.runssl(
 		port,
-		"ssl-keys/server.key",
-		"ssl-keys/server.crt"
+		keydir + "/" + keyfile,
+		keydir + "/" + crtfile
 	)
 except twisted.internet.error.CannotListenError as e:
 	print "Failure to load server. Error follows."
