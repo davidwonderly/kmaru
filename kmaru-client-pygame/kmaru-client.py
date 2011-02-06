c011ba99243889d80eeb0172837e59bc1ee3d0d7
Paul Tagliamonte <paultag@ubuntu.com>
2011-02-06 18:31:49 -0500

    keychecking

--- kmaru-client-pygame/kmaru-client.py
+++ kmaru-client-pygame/kmaru-client.py
@@ -5,12 +5,12 @@ import pygame
 IMG_SRC = "../kmaru-resources/images-copyrighted/UFP.png"
 background_color = (37,51,129)
 
-# define a main function
 def main():
     pygame.init()
     running = True
 
-    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    # screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((600,500))
 
     width  = screen.get_width()
     height = screen.get_height()
@@ -20,18 +20,23 @@ def main():
     image_width  = image.get_width()
     image_height = image.get_height()
 
-    offset_w = (width/2  - (image_width/2))
-    offset_h = (height/4 - (image_height/2))
+    offset_w = (width  / 2 - (image_width  / 2))
+    offset_h = (height / 4 - (image_height / 2))
 
     screen.fill(background_color)
 
-    screen.blit(image, (offset_w,offset_h))
+    screen.blit(image, (offset_w, offset_h))
     pygame.display.flip()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.KEYUP:
-                running = False
+                if event.key == pygame.K_ESCAPE:
+                    print "Quit"
+                    running = False
+                else:
+                    keyinput = chr(event.key)
+                    print keyinput
 
 if __name__=="__main__":
     main()
