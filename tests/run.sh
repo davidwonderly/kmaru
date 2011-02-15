4dd329f2f345edee8e8762e6d7125b55c6536882
Paul Tagliamonte <paultag@ubuntu.com>
2011-02-15 02:06:37 +0000

    using tabs in a clever way

--- tests/run.sh
+++ tests/run.sh
@@ -18,10 +18,10 @@ FAILNESS=0
 for x in `ls *tests*`; do
 	./$x > ./logs/$x.log 2>&1
 	if [ $? -ne 0 ]; then
-		echo "L: $x       [ [31mfail[0m ]"
+		echo -e "L: $x \t\t [ [31mfail[0m ]"
 		let FAILNESS=$FAILNESS+1
 	else
-		echo "L: $x       [  [34mok[0m  ]"
+		echo -e "L: $x \t\t [  [34mok[0m  ]"
 		let GOODNESS=$GOODNESS+1
 	fi
 done
